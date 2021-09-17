319ad7010d8c4c1e3dc485e6a785037fe8fe5612
Esteban Arrangoiz <esteban.arrangoiz@gmail.com>
2021-09-16 22:05:35 -0400

    Finished the solution and all unit tests succeeded

--- Chess/ChessBoard.cs
+++ Chess/ChessBoard.cs
@@ -44,12 +44,60 @@ namespace Chess
 
         public static bool PlaceQueens(ChessBoard board = null, int column = 0)
         {
-            throw new NotImplementedException();
+            board = board?? new ChessBoard();
+
+            for (int row = 1; row <= 8; row++)
+            {
+                board.Board[column] = row;
+
+                if (board.IsSafe())
+                {
+                    if (column == 7)
+                    {
+                        return true; // success
+                    }
+                    else
+                    {
+                        var newBoard = new ChessBoard(board);
+                        if (PlaceQueens(newBoard, column + 1))
+                            return true;
+                        else
+                            continue;
+                    }
+                }
+            }
+
+            return false;
         }
 
         public static bool PlaceQueens(List<ChessBoard> solutions, ChessBoard board = null, int column = 0)
         {
-            throw new NotImplementedException();
+            board = board ?? new ChessBoard();
+            solutions = solutions ?? new List<ChessBoard>(92);
+
+            for (int row = 1; row <= 8; row++)
+            {
+                board.Board[column] = row;
+
+                if (board.IsSafe())
+                {
+                    if (column == 7)
+                    {
+                        solutions.Add(new ChessBoard(board));
+                        return true; // success
+                    }
+                    else
+                    {
+                        var newBoard = new ChessBoard(board);
+                        if (PlaceQueens(solutions, newBoard, column + 1))
+                            continue;
+                        else
+                            continue;
+                    }
+                }
+            }
+
+            return false;
         }
 
         #region Constructors
